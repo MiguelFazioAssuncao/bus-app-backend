ea1bc051f62cdf7501f9d3ebb201c2efea3ee981
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-06-21 21:52:48 -0300

    chore: adicionando a dependencia do cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);   
 app.use("/login", authRoutes);
@@ -12,7 +14,7 @@ app.use("/login", authRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening in http://localhost:${PORT}`);
+  console.log(`🚀 Server listening http://localhost:${PORT}`);
 });
 
 export default app;
